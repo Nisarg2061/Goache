552e346d64e1c6bf9cf2cf8c301e3313f15443e9
Nisarg2061 <khodkenisarg@gmail.com>
2024-08-18 01:24:10 +0530

    Added test files for node add()

---
+++ Node_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+)
+
+func Test_Add(t *testing.T) {
+	c := NewCache()
+	node1 := &Node{Value: "Check"}
+	c.Add(node1)
+
+	// Testing for the position of value added
+	if c.Queue.Head.Right != node1 {
+		t.Errorf("Expected node1 to be at the right of head got %v", c.Queue.Head.Right)
+	}
+
+	// Exciding limit
+	for i := 0; i < SIZE; i++ {
+		chechNode := &Node{Value: string("c" + strconv.Itoa(i))}
+		c.Add(chechNode)
+	}
+
+	if c.Queue.Tail.Left.Value == "Check" {
+		t.Errorf("Expected node1 to be removed but it didn't.\n")
+	}
+}
