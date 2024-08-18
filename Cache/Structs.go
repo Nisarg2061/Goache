a59e0777c538318d6f533012a5f58b56789b07c1
Nisarg2061 <khodkenisarg@gmail.com>
2024-08-18 23:55:29 +0530

    Re-structured files

--- Cache/Structs.go
+++ Cache/Structs.go
@@ -1,4 +1,4 @@
-package main
+package Cache
 
 const SIZE = 5
 
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Nisarg2061/Goache/Cache"
 )
 
 func main() {
 	fmt.Println("Start Cache...")
-	cache := NewCache()
+	cache := Cache.NewCache()
 	listOfItems := []string{"Cat", "Dog", "Turtle", "Mouse", "Bat", "Dog", "Nisarg", "Fox", "Tiger"}
 
 	for _, val := range listOfItems {
